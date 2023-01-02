1d9a8717c578d463305c3be32285d3665295d42b
alisalamatian1 <alisalamatian1@gmail.com>
2023-01-02 20:20:34 +0000

    pm2 restart ws

--- run.sh
+++ run.sh
@@ -3,9 +3,10 @@
 pm2 kill;
 pm2 list;
 cd Server;
-./node_modules/.bin/pm2 --name node start app.ts;
+npm run build
+./node_modules/.bin/pm2 --name node app.ts --watch;
 pm2 save --force
 pm2 list;
 cd ..;
-pm2 --name ws start WebSocket/server.js;
+pm2 --name ws watch WebSocket/server.js;
 pm2 list;
