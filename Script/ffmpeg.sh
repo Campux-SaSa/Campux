d404652542e965b803901459fff6f73f020941e9
alisalamatian1 <alisalamatian1@gmail.com>
2023-01-03 06:13:59 +0000

    agi.mp4

--- Script/ffmpeg.sh
+++ Script/ffmpeg.sh
@@ -2,4 +2,4 @@
 
 # -fflags +genpts -stream_loop -1 is for 
 # this needs to be copied and pasted in terminal to work right, don't forget the '&'
-ffmpeg -re -fflags +genpts -stream_loop -1 -i tyson.mp4 -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost:1935/live/rfBd56ti2SMtYvSgD5xAV0YU99zampta7Z7S575KLkIZ9PYk
+ffmpeg -re -fflags +genpts -stream_loop -1 -i agi.mp4 -c:v libx264 -preset veryfast -tune zerolatency -c:a aac -ar 44100 -f flv rtmp://localhost:1935/live/rfBd56ti2SMtYvSgD5xAV0YU99zampta7Z7S575KLkIZ9PYk &
